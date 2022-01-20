7b76f4179a43aa3a79d8220e5f620b24654802ca
chiater0311 <chiater0311@gmail.com>
2022-01-20 17:04:54 +0800

    transforming scale and opacity

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -1,10 +1,10 @@
 <template>
     <layout>
         <div id="bg-video" class="w-full h-full bg-contain bg-no-repeat bg-center bg-neutral-800">
-            <div class="absolute right-1/2 flex flex-col items-center w-40">
+            <div id="model" class="absolute right-1/2 flex flex-col items-center w-40">
                 
-                <img id="personImg" class="w-20" />
-                <img id="cardNameImg" class="w-full h-6" />
+                <div id="personImg" class="w-20"></div>
+                <div id="cardNameImg" class="w-full h-6"></div>
                 
             </div>
             <!-- <div class="cardName-container">
@@ -22,23 +22,54 @@
     }
 
     #personImg{
-        content:url("/images/person1.jpg");
+        content: url("/images/person1.jpg");
+        /* animation: flow 10s linear; */
     }
 
     #cardNameImg{
         content:url("/images/card.Png");
+        /* animation: flow 10000s linear infinite; */
         
+    }
+
+    #model{
+        animation: flow 10s linear;
+    }
+
+    @keyframes flow {
+        0% {
+            transform: scale(0);
+            opacity: 0;
+        }
+
+        25% {
+            transform: scale(0.25);
+            opacity: 0.25;
+        }
+
+        50% {
+            transform: scale(0.50);
+            opacity: 0.50;
+        }
+
+        75% {
+            transform: scale(0.75);
+            opacity: 0.75;
+        }
+
+        100% {
+            transform: scale(1);
+            opacity: 1;
+        }
     }
 </style>
 
 <script>
     import Layout from '@/Shared/Layout'
-    import VideoBg from 'vue-videobg'
     export default {
         props: ['foo'],
             components: {
-                Layout,
-                VideoBg:'video-bg'
+                Layout
             },
         }
 </script>
